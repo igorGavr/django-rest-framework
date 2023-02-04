0f360de43342f3114dbe97101aa26c29ed858bc4
igorGavr <ragamuffinrogi@gmail.com>
2023-02-04 19:44:27 +0200

    products

--- api/views.py
+++ api/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
-# Create your views here.
+
+from products.models import Product
+from django.forms.models import model_to_dict
+
 
 def api_home(request, *args, **kwargs):
-    body = request.body
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.load(body)
-    except:
-        pass
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "title"])
     print(data)
-    print(request.headers)
-    data['content_type'] = request.content_type
-    return JsonResponse(data)
+    return HttpResponse(data)
+
+
+
+# def api_home(request, *args, **kwargs):
+#     print(request.GET)
+#     print(request.POST)
+#     body = request.body
+#     data = {"con":"hi"}
+#     try:
+#         data = json.load(body)
+#     except:
+#         pass
+#
+#     data["params"] = dict(request.GET)
+#     data['headers'] = dict(request.headers)
+#     data['content_type'] = request.content_type
+#     print(data)
+#     return JsonResponse(data)
