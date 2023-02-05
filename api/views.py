4607a3cc9f307484c6e82b556e661eb1f130395a
igorGavr <ragamuffinrogi@gmail.com>
2023-02-05 16:12:59 +0200

    serializers

--- api/views.py
+++ api/views.py
@@ -1,33 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import json
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from products.models import Product
 from django.forms.models import model_to_dict
+from products.serializers import ProductSerializer
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "title"])
-    print(data)
-    return HttpResponse(data)
+
+    instance = Product.objects.all().order_by("?").first()
+    date = {}
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
 
 
 
-# def api_home(request, *args, **kwargs):
-#     print(request.GET)
-#     print(request.POST)
-#     body = request.body
-#     data = {"con":"hi"}
-#     try:
-#         data = json.load(body)
-#     except:
-#         pass
-#
-#     data["params"] = dict(request.GET)
-#     data['headers'] = dict(request.headers)
-#     data['content_type'] = request.content_type
-#     print(data)
-#     return JsonResponse(data)
