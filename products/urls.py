fe74e63f5d3f62951568e1582cdb9051ca4a5b0c
igorGavr <ragamuffinrogi@gmail.com>
2023-02-07 09:51:14 +0200

    one

--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProductListCreateView.as_view()),
-    path('<int:pk>/', views.ProductRetrieveUpdateDestroyView.as_view())
-]
